5be10fce83c70e3e292408642e352c3794468549
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-14 13:19:13 +0545

    routing of login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app.routes.jsx'
 
 function App() {
   return (
-    <div>App</div>
+
+    <RouterProvider router={router} />
   )
 }
 
