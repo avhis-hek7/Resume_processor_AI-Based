74e908222a7e99c0e13307c385fba87ad0c0902f
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-15 07:26:59 +0545

    create context for setusers and laoding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app.routes.jsx'
+import { AuthProvider } from './features/auth/usecontext/auth.context.jsx'
 
 function App() {
   return (
-
-    <RouterProvider router={router} />
+<AuthProvider>
+     <RouterProvider router={router} />
+</AuthProvider>
+    
   )
 }
 
---
+++ frontend/src/features/auth/usecontext/auth.context.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import AuthContext from "./createContext";
+
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <AuthContext.Provider value={{user, setUser, loading, setLoading}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
