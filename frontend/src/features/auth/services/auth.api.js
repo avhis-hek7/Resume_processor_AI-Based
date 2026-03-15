e4c85aa5804b2db414d401afc46c66c16623b0c4
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-15 07:14:04 +0545

    create a register, login, logout and getMe function to communicate with backend and axios instance for repetative code

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+const api = axios.create({
+    baseUrl: "http://localhost:3000",
+    withCredentials:true
+})
+
 export async function register(username,email,password){
 
     try {
-        const response = axios.post('http://localhost:3000/api/auth/register',
-        {username, email, password},
-        {withCredentials:true}
+        const response = await api.post('/api/auth/register',
+        {username, email, password}
+    
     )
 
     return response.data
@@ -14,4 +19,36 @@ export async function register(username,email,password){
         console.log(error);
     }
     
+}
+
+export async function login(email,password){
+    try {
+        const response = await api.post('/api/auth/login',
+            {email, password}
+            
+        )
+        return response.data
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+export async function logout(){
+    try {
+        const response = await api.get('/api/auth/logout')
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+export async function getMe(){
+    try {
+        const response = await axios.get('/api/auth/get-me')
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
 }
