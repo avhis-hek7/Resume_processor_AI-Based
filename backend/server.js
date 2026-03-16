a63b841f24034540fc4c872a7acc26c34db587bd
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-16 09:45:02 +0545

    interviewreport ai model create , test and it work correctly

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDb = require('./src/config/db');
 
+const invokeGeminiAi = require('./src/services/ai.service');
+
+
+
 
 connectDb();
+invokeGeminiAi();
+
+
+
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
