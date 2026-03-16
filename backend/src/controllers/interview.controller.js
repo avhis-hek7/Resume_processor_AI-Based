f037cfb810c53c0a192070f57cd41dd09707d0f0
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-16 13:10:40 +0545

    ai error

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -5,18 +5,18 @@ const candidateInterviewModel = require('../models/interviewReport.model')
 async function generateInterViewReportController(req,res){
    
 
-    const resumeContent = pdfParse(req.file.buffer);
+     const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
     const {selfDescription, jobDescription} = req.body;
 
     const interviewReportByAi = await generateInterviewReport({
-        resume:resumeContent,
+        resume:resumeContent.text,
         selfDescription,
         jobDescription
     })
 
     const interviewReport = await candidateInterviewModel.create({
         user: req.user.userId,
-        resume:resumeContent,
+        resume:resumeContent.text,
         selfDescription,
         jobDescription,
         ...interviewReportByAi
